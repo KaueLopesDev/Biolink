f4b55428fa3ccf777f1378120220ecc454af1ce4
KaueLopesDev <242053721+KaueLopesDev@users.noreply.github.com>
2026-07-18 18:32:09 -0300

    Site V2
    Correção de bug de animação é Fundo modificado

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function copiarTexto() {
 })
 
 }
+
+
+window.addEventListener("load", () => {
+const video = document.createElement('video');
+const container = document.querySelector(".midiaBackground");
+
+video.src = "video/Fundo.mp4";
+video.autoplay = true;
+video.muted = true;
+video.playsInline = true;
+video.loop = true;
+video.style.opacity = '0';
+
+video.addEventListener("canplaythrough", () => {
+video.style.opacity = "1";
+});
+
+midiaBackground.appendChild(video);
+});
